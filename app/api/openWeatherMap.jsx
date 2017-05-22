fb45117cbb873b11de16b986ea5f51d12102c71f
Adam Perlstrom <adam.perlstrom@hotmail.co.uk>
2017-05-22 13:51:26 +0100

    app working with base styles added

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a42ffe692ff447c98f9d6433780c18a2&units=imperial';
 
 module.exports = {
   getTemp: function (location) {
